b696216dc5d6b6ca5db52db7cd06a18904860d5f
mathunes <matheusantunes720@gmail.com>
2023-03-21 21:41:02 -0300

    each loop function to reduce function

--- lib/services/cr_calculator_service.rb
+++ lib/services/cr_calculator_service.rb
@@ -1,18 +1,16 @@
 class CrCalculatorService
     def self.calculate_cr_by_register_clustered(register)
-        total_credit_hours = 0
-        sum_of_multiplication_grade_credit_hours = 0
-
-        register.each do |discipline_cursed|
-            discipline = discipline_cursed[0]
-            enrollment = discipline_cursed[1]
-            student = discipline_cursed[2]
-
-            total_credit_hours += discipline.credit_hours.to_i
-
-            sum_of_multiplication_grade_credit_hours += enrollment.grade.to_i * discipline.credit_hours.to_i
-        end
+        total_credit_hours = calculate_total_credit_hours(register)
+        sum_of_multiplication_grade_credit_hours = calculate_sum_of_multiplication_grade_credit_hours(register)
 
         sum_of_multiplication_grade_credit_hours / total_credit_hours
+    end
+
+    def self.calculate_total_credit_hours(register)
+        register.reduce(0) { |sum, discipline_cursed| sum + discipline_cursed[0].credit_hours.to_i }        
+    end
+
+    def self.calculate_sum_of_multiplication_grade_credit_hours(register)
+        register.reduce(0) { |sum, discipline_cursed| sum + (discipline_cursed[1].grade.to_i * discipline_cursed[0].credit_hours.to_i) }
     end
 end
